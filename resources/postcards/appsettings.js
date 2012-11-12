5c664adee2d68130d4d2b8e38ddf56ade1dc2dd8
Cameron Browning <camb416@gmail.com>
2012-11-12 15:42:04 -0500

    oops, data folder shouldn't be under source control

--- resources/postcards/appsettings.js
+++
@@ -1,9 +0,0 @@
-{
-    "debugstate"    :   0,
-    "timeout"       :   60,
-    "elementspath"  :   "designElements/",
-	"instructions"  :   [
-    "instructions1.png"
-    ],
-    "background"    :   "background.jpg"
-}
